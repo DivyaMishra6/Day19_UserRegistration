0194402a7b22d629d49af356f585323e7d1a96ee
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-10 23:49:47 +0530

    Valid Last Name

---
+++ ValidLastNameUC2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19_UserRegistration
+{
+    public class ValidLastNameUC2
+    {
+        public static string FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public static string LastName = "^[A-Z]{1}[A-Za-z]{2,}$";
+
+        //CHECK 1ST NAME
+        public bool ValidateFName(string FName)
+        {
+            return Regex.IsMatch(FName, FirstName);
+        }
+
+
+        //CHECK LAST NAME
+        public bool ValidateLName(string LName)
+        {
+            return Regex.IsMatch(LName, FirstName);
+        }
+    }
+}
