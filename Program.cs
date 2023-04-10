2fb506172bc5ef1d06319fb2a80d48169377e169
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-10 23:51:25 +0530

    Check valid Last Name

--- Program.cs
+++ Program.cs
@@ -10,9 +10,14 @@ namespace UserRegistration
 
             Console.WriteLine("********************************");
             UserRegistrationCodeUC1 obj = new UserRegistrationCodeUC1 ();
+            ValidLastNameUC2 obj1 = new ValidLastNameUC2 ();
             Console.WriteLine("Enter the First Name:");
             string FName = Console.ReadLine();
+            Console.WriteLine("Enter the Last Name:");
+            string LName = Console.ReadLine();
+
             Console.WriteLine(obj.ValidateFName(FName));
+            Console.WriteLine(obj.ValidateFName(LName));
         }
 
     }
