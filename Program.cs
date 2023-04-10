fbe4d970e4c9ab4fa5aa5f9f0517a1113cb732c8
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-10 21:01:50 +0530

    Check valid First name

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace UserRegistration
+﻿using Day19_UserRegistration;
+
+namespace UserRegistration
 {
     public class Program
     {
@@ -6,7 +8,11 @@
         {
             Console.WriteLine("Welcome to User Registration Page!!");
 
-
+            Console.WriteLine("********************************");
+            UserRegistrationCodeUC1 obj = new UserRegistrationCodeUC1 ();
+            Console.WriteLine("Enter the First Name:");
+            string FName = Console.ReadLine();
+            Console.WriteLine(obj.ValidateFName(FName));
         }
 
     }
