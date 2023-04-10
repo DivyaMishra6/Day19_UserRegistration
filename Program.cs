64b880a28d30a7954d94c50932e12c7137502924
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-11 00:10:29 +0530

    Check valid email Address

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace UserRegistration
+﻿using Day19_UserRegistration;
+
+namespace UserRegistration
 {
     public class Program
     {
@@ -6,7 +8,19 @@
         {
             Console.WriteLine("Welcome to User Registration Page!!");
 
+            Console.WriteLine("Welcome to User Registration Page!!");
+            Console.WriteLine("********************************");
+            ValidEmailUC3 obj = new ValidEmailUC3();
+            Console.WriteLine("Enter the First Name:");
+            string FName = Console.ReadLine();
+            Console.WriteLine("Enter the Last Name:");
+            string LName = Console.ReadLine();
+            Console.WriteLine("Enter your Email Id:");
+            string MailId = Console.ReadLine();
 
+            Console.WriteLine(obj.ValidateFName(FName));
+            Console.WriteLine(obj.ValidateFName(LName));
+            Console.WriteLine(obj.ValidateEmail(MailId));
         }
 
     }
