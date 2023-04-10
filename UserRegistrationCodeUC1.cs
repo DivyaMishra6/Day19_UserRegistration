635a6bdfc439088731d81fd95dc6cdd6aa1d582a
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-10 21:03:10 +0530

    Valid First Name

---
+++ UserRegistrationCodeUC1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day19_UserRegistration
+{
+    public class UserRegistrationCodeUC1
+    {
+        public static string FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public bool ValidateFName(string FName)
+        {
+            return Regex.IsMatch(FName, FirstName);
+        }
+    }
+}
