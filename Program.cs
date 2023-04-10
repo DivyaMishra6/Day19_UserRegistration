a49c3d2bca47dfee5b2d72f414ada4fdf7ddb716
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-11 00:39:33 +0530

    Valid Mobile Number

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace UserRegistration
+﻿using Day19_UserRegistration;
+
+namespace UserRegistration
 {
     public class Program
     {
@@ -6,7 +8,21 @@
         {
             Console.WriteLine("Welcome to User Registration Page!!");
 
+            Console.WriteLine("********************************");
+            ValidMobileNumberUC4 obj = new ValidMobileNumberUC4();
+            Console.WriteLine("Enter the First Name:");
+            string FName = Console.ReadLine();
+            Console.WriteLine("Enter the Last Name:");
+            string LName = Console.ReadLine();
+            Console.WriteLine("Enter your Email Id:");
+            string MailId = Console.ReadLine();
+            Console.WriteLine("Enter your Mobile Number:");
+            string MobNum = Console.ReadLine();
 
+            Console.WriteLine(obj.ValidateFName(FName));
+            Console.WriteLine(obj.ValidateFName(LName));
+            Console.WriteLine(obj.ValidateEmail(MailId));
+            Console.WriteLine(obj.ValidateMobNum(MobNum));
         }
 
     }
