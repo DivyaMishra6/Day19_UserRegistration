1115ca772fa98d0d29085da69e673a2e06027312
DivyaMishra6 <divyamishra599@gmail.com>
2023-04-11 15:42:14 +0530

    Email sample

--- Program.cs
+++ Program.cs
@@ -9,22 +9,32 @@ namespace UserRegistration
         {
             Console.WriteLine("Welcome to User Registration Page!!");
             Console.WriteLine("********************************");
-            PasswordRule3UC7 obj = new PasswordRule3UC7();
-            Console.WriteLine("Enter the First Name:");
-            string FName = Console.ReadLine();
-            Console.WriteLine("Enter the Last Name:");
-            string LName = Console.ReadLine();
+             EmailSampleUC9 obj = new EmailSampleUC9();
+            //Console.WriteLine("Enter the First Name:");
+            //string FName = Console.ReadLine();
+            //Console.WriteLine("Enter the Last Name:");
+            //string LName = Console.ReadLine();
+            //Console.WriteLine("Enter your Email Id:");
+            //string MailId = Console.ReadLine();
+            //Console.WriteLine("Enter your Mobile Number:");
+            //string MobNum = Console.ReadLine();
+            //Console.WriteLine("Enter your password:");
+            //string password = Console.ReadLine();
+            //Console.WriteLine(obj.ValidateFName(FName));
+            //Console.WriteLine(obj.ValidateFName(LName));
+            //Console.WriteLine(obj.ValidateEmail(MailId));
+            //Console.WriteLine(obj.ValidateMobNum(MobNum));
+            //Console.WriteLine(obj.ValidatePassword(password));
+
             Console.WriteLine("Enter your Email Id:");
             string MailId = Console.ReadLine();
-            Console.WriteLine("Enter your Mobile Number:");
-            string MobNum = Console.ReadLine();
-            Console.WriteLine("Enter your password:");
-            string password = Console.ReadLine();
-            Console.WriteLine(obj.ValidateFName(FName));
-            Console.WriteLine(obj.ValidateFName(LName));
-            Console.WriteLine(obj.ValidateEmail(MailId));
-            Console.WriteLine(obj.ValidateMobNum(MobNum));
-            Console.WriteLine(obj.ValidatePassword(password));
+
+            string[] emailSamples = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com" };
+
+            foreach (string email in emailSamples)
+            {
+                Console.WriteLine("{0} : {1}", email, obj.ValidateEmail(MailId) ? "Valid" : "Invalid");
+            }
         }
     }
 }
